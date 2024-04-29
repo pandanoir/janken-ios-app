2da15746eb51466fb5882b410e6052aec8fc3a8e
pandanoir <pandanoir@ymail.ne.jp>
2024-04-29 17:06:51 +0900

    Initial Commit

---
+++ janken/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  janken
+//
+//  Created by 幾野直人 on 2024/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ janken/jankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  jankenApp.swift
+//  janken
+//
+//  Created by 幾野直人 on 2024/04/29.
+//
+
+import SwiftUI
+
+@main
+struct jankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
